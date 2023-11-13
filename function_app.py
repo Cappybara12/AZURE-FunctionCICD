380efdb551e3936f9357b6bc33485505d6b47f95
Akshay Sharma <akshayne912@gmail.com>
2023-11-13 17:11:14 +0530

    made changes in fucntion _app.py

--- function_app.py
+++ function_app.py
@@ -2,8 +2,18 @@ import azure.functions as func
 from azure.cosmos import CosmosClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    headers = {
+        'Access-Control-Allow-Origin': '*',  # or specify your allowed origins
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',  # Add any other needed methods
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+
+    # Check for preflight request
+    if req.method == 'OPTIONS':
+        return func.HttpResponse(headers=headers)
+
     endpoint = "https://portfoliodbmlsa.table.cosmos.azure.com:443/"
-    #need to find key
+    # need to find key
     key = "UUEUax1TywwbwKzPSolleubVvCKS1e2TihbPTimmyJMGTjKp0hbra39O25lsoygrFvKB4wPnm0vwACDbqZTn0A=="
     database_name = "portfoliodbmlsa"
     container_name = "table1"
@@ -21,4 +31,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     container.upsert_item({'id': 'uniqueCounter', 'count': count + 1})
 
     # Respond with the updated count
-    return func.HttpResponse(f"Visitor Count: {count + 1}")
+    return func.HttpResponse(f"Visitor Count: {count + 1}", headers=headers)
